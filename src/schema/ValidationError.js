86a9a5e22868d0f8f058605951dbd4fbc9d4d248
zhifei <zhifei@zhifeideMacBook-Pro.local>
2017-06-26 20:25:27 +0800

    feat: add ValidationError subclass.

---
+++ src/schema/ValidationError.js
@@ -0,0 +1,20 @@
+const { GraphQLError } = require('graphql');
+
+class ValidationError extends GraphQLError {
+  constructor(errors) {
+    super('The request is invalid.');
+    this.state = errors.reduce((result, error) => {
+      if (Object.prototype.hasOwnProperty.call(result, error.key)) {
+        result[error.key].push(error.message);
+      } else {
+        Object.defineProperty(result, error.key, {
+          value: [error.message],
+          enumerable: true,
+        });
+      }
+      return result;
+    }, {});
+  }
+}
+
+module.exports = ValidationError;
